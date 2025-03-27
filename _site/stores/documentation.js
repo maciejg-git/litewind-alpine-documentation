f033a30f817e3bacd7b25460d602d5f15c8d27d6
maciejg-git <m.gralek@gmail.com>
2025-03-27 22:18:49 +0100

    update

--- _site/stores/documentation.js
+++ _site/stores/documentation.js
@@ -5,16 +5,20 @@ document.addEventListener('alpine:init', () => {
     init() {
       if (!this.darkMode) {
         document.documentElement.classList.add("dark")
+        document.documentElement.style.setProperty("color-scheme", "dark")
       } else {
         document.documentElement.classList.remove("dark")
+        document.documentElement.style.setProperty("color-scheme", "light")
       }
     },
     toggle() {
       this.darkMode = !this.darkMode
       if (!this.darkMode) {
         document.documentElement.classList.add("dark")
+        document.documentElement.style.setProperty("color-scheme", "dark")
       } else {
         document.documentElement.classList.remove("dark")
+        document.documentElement.style.setProperty("color-scheme", "light")
       }
     }
   })
